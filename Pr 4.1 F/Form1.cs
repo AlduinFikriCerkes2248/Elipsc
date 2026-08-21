92ede3ec48fcae5f3b00f9b6bc003fcf9ce66037
Arzubov Maksum <maksimarzubov491@gmail.com>
2026-08-21 11:31:40 +0300

    Add project files.

---
+++ Pr 4.1 F/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr_4._1_F
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen myPen = new Pen(Color.Aqua, 3);
+            Rectangle r = new Rectangle(123, 24, 200, 300);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.DrawEllipse(myPen, r);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pen myPen = new Pen(Color.Red, 3);
+            Rectangle r = new Rectangle(123, 24, 200, 300);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.DrawRectangle(myPen, r);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SolidBrush myBrush = new SolidBrush(Color.Aqua);
+            Rectangle r = new Rectangle(123, 24, 200, 300);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.FillEllipse(myBrush, r);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SolidBrush myBrush = new SolidBrush(Color.Red);
+            Rectangle r = new Rectangle(123, 24, 200, 300);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.FillRectangle(myBrush, r);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+    }
+}
---
+++ Pr 4.1 F/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Arzubov 1p-24
+namespace Pr_4._1_F
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double radius = Convert.ToDouble(txtRadius.Text);
+            double area = Math.PI * Math.Pow(radius, 2);
+            txtArea.Text = area.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double radius = Convert.ToDouble(txtRadius.Text);
+            int x = Convert.ToInt32(txtX.Text);
+            int y = Convert.ToInt32(txtY.Text);
+            int diameter = Convert.ToInt32(radius * 2);
+
+            Pen myPen = new Pen(Color.Black, 3);
+            Graphics g = Graphics.FromHwnd(this.Handle);
+
+            g.DrawEllipse(myPen, x, y, diameter, diameter);
+        }
+
+        private void txty_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+        }
+    }
+}
